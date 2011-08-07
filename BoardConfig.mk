345793ff691d8682850fc630be85cc6160170fe0
jerpelea <jerpelea@gmail.com>
2011-08-07 09:23:07 +0300

    semc: added Xperia X10 Mini (robyn)

---
+++ BoardConfig.mk
@@ -0,0 +1,6 @@
+-include device/semc/msm7x27-common/BoardConfigCommon.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := robyn
+TARGET_SPECIFIC_HEADER_PATH := device/semc/robyn/include
+
+-include device/semc/msm7x27-common/Android.mk
