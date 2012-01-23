324644048f7653df42fa7805c7bb27561e4557b0
freexperia <jerpelea@gmail.com>
2012-01-23 17:25:59 +0200

    semc: updated with latest kernel and cleanup
    Change-Id: If48427be630fd752d42a2794012aa2230f8c1331

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,7 @@
 -include device/semc/msm7x27-common/BoardConfigCommon.mk
 -include vendor/semc/robyn/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := robyn
-TARGET_SPECIFIC_HEADER_PATH := device/semc/robyn/include
-
-BOARD_CUSTOM_GRAPHICS := ../../../device/semc/msm7x27-common/recovery/graphics.c
+TARGET_BOOTLOADER_BOARD_NAME := delta
 
 TARGET_OTA_ASSERT_DEVICE := E10i,E10a,robyn
 
--- device_robyn.mk
+++ device_robyn.mk
@@ -7,18 +7,9 @@ $(call inherit-product-if-exists, vendor/semc/robyn/robyn-vendor.mk)
 
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := E10i
+PRODUCT_NAME := Xperia X10 Mini
 PRODUCT_DEVICE := robyn
 PRODUCT_MODEL := E10i
-
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-LOCAL_KERNEL := device/semc/robyn/kernel
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
 
 -include device/semc/msm7x27-common/msm7x27.mk
 
@@ -28,7 +19,7 @@ PRODUCT_COPY_FILES += \
 DEVICE_PACKAGE_OVERLAYS += device/semc/robyn/overlay
 
 PRODUCT_PACKAGES += \
-    lights.robyn 
+    lights.delta
 
 # These are the hardware-specific configuration files
 PRODUCT_COPY_FILES += \
@@ -40,7 +31,6 @@ PRODUCT_COPY_FILES += \
 # Init files
 PRODUCT_COPY_FILES += \
     device/semc/robyn/prebuilt/bootrec:root/sbin/bootrec
-
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=120 \
@@ -57,5 +47,5 @@ PRODUCT_COPY_FILES += \
     device/semc/msm7x27-common/prebuilt/animations/anim5.rle:system/etc/semc/chargemon/anim5.rle \
     device/semc/msm7x27-common/prebuilt/animations/anim6.rle:system/etc/semc/chargemon/anim6.rle \
     device/semc/msm7x27-common/prebuilt/animations/anim7.rle:system/etc/semc/chargemon/anim7.rle \
-    device/semc/msm7x27-common/prebuilt/animations/anim8.rle:system/etc/semc/chargemon/anim8.rle 
+    device/semc/msm7x27-common/prebuilt/animations/anim8.rle:system/etc/semc/chargemon/anim8.rle
 
